bf6949bc1f9bb3f24126a88eac39238e3f9eb642
Pablitocfx <julianxchavez24@gmail.com>
2025-02-20 01:05:23 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.UseAdminIdentifier = true
+Config.UseAdminGroup = false
+
+Config.AdminIdentifiers = {   -- notes i made this super quick steam dont work as of now groups work perfect
+    "steam:11000013a0b0a0a", -- Example
+}
+
+Config.AdminGroups = {
+    "god",
+    "admin" -- Example
+}
+
